728edcb1fb8f1ea0098518d9f8b00aba6a563915
akshaypd5 <akshaypadale2305@gmail.com>
2025-04-30 11:31:05 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "myregion" {
+  description = "my region"
+}
+variable "myaccesskey" {
+  description = "my access key"
+}
+variable "mysecretkey" {
+  description = "my secret key"
+}
+variable "mybucket" {
+  description = "my bucket"
+  default = "my-test-bucket"
+}
